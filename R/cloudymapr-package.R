1763c29256fdda77c5f05a3dc84ff8d3d5a17b13
Ben Raymond <ben.raymond@aad.gov.au>
2026-02-17 11:37:40 +1100

    drop abind, leaflet imports, unigd back to CRAN version

--- R/cloudymapr-package.R
+++ R/cloudymapr-package.R
@@ -5,11 +5,9 @@
 #' @name cloudymapr
 #' @aliases cloudymapr-package
 #' @docType package
-#' @importFrom abind abind
 #' @importFrom dplyr across bind_cols bind_rows everything mutate tibble tribble
 #' @importFrom graphics par points text
 #' @importFrom grDevices col2rgb dev.off hcl.colors png
-#' @importFrom leaflet addTiles addWMSTiles leaflet leafletCRS leafletOptions setView tileOptions WMSTileOptions
 #' @importFrom magrittr %>%
 #' @importFrom methods new
 #' @importFrom sf st_as_sf st_coordinates `st_crs<-` st_geometry st_transform
